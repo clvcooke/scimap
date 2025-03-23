d3fd4b78c85282fabd81d3deb6174374d727da23
Colin Cooke <colin@animahealth.com>
2025-03-23 15:56:46 -0400

    fix fonts and share

--- src/components/Advocacy.tsx
+++ src/components/Advocacy.tsx
@@ -3,12 +3,14 @@ import {
     Title,
     Text,
     Button,
-    Card,
+    Card, Modal,
 } from '@mantine/core';
-import { IconExternalLink } from '@tabler/icons-react';
+import {IconExternalLink, IconShare} from '@tabler/icons-react';
 import SharePage from "./SharePage.tsx";
+import { useState } from 'react';
 
 function Advocacy() {
+    const [showShare, setShowShare] = useState(false);
     return (
         <Container size="md">
             <Title ta="center" mt="xl" mb="md">
@@ -21,12 +23,12 @@ function Advocacy() {
 
             <Card withBorder shadow="sm" mb="lg">
                 <Card.Section withBorder p="md">
-                    <Text fw={700} size="md">
+                    <Text fw={700} size="lg">
                         Contact Your Representatives
                     </Text>
                 </Card.Section>
                 <Card.Section p="md">
-                    <Text size="sm" mb="sm" ta={'left'}>
+                    <Text size="md" mb="sm" ta={'left'}>
                         We encourage you to <b>contact your representatives</b> in Congress to share your opinion and call attention to these funding cuts
                     </Text>
                     <Button
@@ -43,18 +45,23 @@ function Advocacy() {
 
             <Card withBorder shadow="sm" mb="lg">
                 <Card.Section withBorder p="md">
-                    <Text fw={700} size="md">
+                    <Text fw={700} size="lg">
                         Share this information
                     </Text>
                 </Card.Section>
                 <Card.Section p="md">
-                    <Text size="sm" mb="sm" ta={'left'}>
+                    <Text size="md" mb="sm" ta={'left'}>
                         We also encourage you to share our website with friends, family, and followers
                     </Text>
-                    <SharePage />
+                    <Button rightSection={<IconShare size={16}/>} onClick={() => setShowShare(true)}>Share</Button>
+
                 </Card.Section>
             </Card>
-
+            <Modal withinPortal={false} opened={showShare} onClose={() => setShowShare(false)} withCloseButton={false} centered>
+                <SharePage
+                    title={"See national impact of federal health research cuts"}
+                />
+            </Modal>
 
         </Container>
     );
