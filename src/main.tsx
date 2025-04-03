a3ebcc013e2efa3f7ca89592dadb16a2aa5dde7b
Colin Cooke <colin@animahealth.com>
2025-04-02 21:08:41 -0400

    remove posthog

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,10 @@ import App from './App.tsx'
 import {MantineProvider} from "@mantine/core";
 import '@mantine/core/styles.css';
 import {Helmet} from "react-helmet";
-import {PostHogProvider} from 'posthog-js/react'
-import {REACT_APP_PUBLIC_POSTHOG_HOST, REACT_APP_PUBLIC_POSTHOG_KEY} from "./constants.ts";
 
-const options = {
-    api_host: REACT_APP_PUBLIC_POSTHOG_HOST,
-}
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <PostHogProvider
-            apiKey={REACT_APP_PUBLIC_POSTHOG_KEY}
-            options={options}>
-
             <MantineProvider>
                 <Helmet>
                     <title>SCIMaP</title>
@@ -25,6 +16,5 @@ createRoot(document.getElementById('root')!).render(
                 </Helmet>
                 <App/>
             </MantineProvider>
-        </PostHogProvider>
     </StrictMode>,
 )
