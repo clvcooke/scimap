76a91b48608039d8a2bb2357283df7ade8ff1e00
Colin Cooke <colin@animahealth.com>
2025-04-02 21:27:01 -0400

    sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App.tsx'
 import {MantineProvider} from "@mantine/core";
 import '@mantine/core/styles.css';
 import {Helmet} from "react-helmet";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://5cc6625eac8a4075e42341deefc42135@o4509086741889024.ingest.us.sentry.io/4509086742872064"
+});
 
 
 createRoot(document.getElementById('root')!).render(
