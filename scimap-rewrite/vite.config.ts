ccfbb208f7e42511407811e4c9ba1d7e1e58a1e7
clvcooke <colin@animahealth.com>
2026-03-22 20:24:17 -0400

    Change vite config

--- scimap-rewrite/vite.config.ts
+++ scimap-rewrite/vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/scimap-rewrite/',
   plugins: [
     tailwindcss(),
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
